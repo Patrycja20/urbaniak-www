10d8175792623eedbb650e1ec1f53030320ef9b6
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2023-04-14 17:40:31 +0200

    kolejne zmiany, texty

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', event => {
             return;
         }
         if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+//            navbarCollapsible.classList.remove('navbar-shrink')
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
@@ -55,5 +55,7 @@ window.addEventListener('DOMContentLoaded', event => {
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
     });
+
+    n
 
 });
